80ddfdc326d257a1c07ce4feb885ec654896e269
weigang <weigang@genectr.hunter.cuny.edu>
2020-03-18 15:42:28 -0400

    hapnet.pl: codon strcuture corrected & validated. previous version has the wrong BED file & didn't consider orf1ab coding shift

--- hapnet.pl
+++ hapnet.pl
@@ -5,8 +5,7 @@
 # Output: JSON for D3-force rendering
 # Done (3/15/2020):  add additional attributes including gene, codon, AA, syn/nonsyn (by parsing VCF & BED file)
 # Done (3/15/2020):  uniq_seq: consolidate sequences with internal missing bases; solved bioaln --gap-char "n" (not ideal)
-# TO DO:
-#     outgroup rooting: re-orient MSP path (Single-Source Shortest Paths, SSSP, won't work because it doesn't cover all nodes)
+# Done (3/16/2020):  outgroup rooting: re-orient MSP path (Single-Source Shortest Paths, SSSP, won't work because it doesn't cover all nodes)
 #
 # Perl modules for Graphs: https://metacpan.org/release/Graph
 #
@@ -17,7 +16,6 @@ use Data::Dumper;
 use Graph;
 use Graph::Writer::Dot;
 use Graph::Traversal::DFS;
-use Graph::Traverse;
 use Bio::SeqFeature::Generic;
 use JSON;
 use Getopt::Long;
@@ -27,8 +25,10 @@ use Bio::Tools::CodonTable;
 my $myCodonTable   = Bio::Tools::CodonTable->new();
 my $writer = Graph::Writer::Dot->new();
 #die "Usage: $0 --hap <fasta> --vcf <vcf> --genome <fasta>\n" unless;
+# e.g.: perl hapnet.pl --vcf snps-ref.vcf --genome ref.gb --hap samples4.aln  --output json  > net.json 2> tmp.log
 my %options;
 my $outgroup = 'EPI_ISL_402131';
+my $orfShift = 13468; # orf1ab reading frame shift
 GetOptions (
     \%options,
     "hap=s",
@@ -36,6 +36,7 @@ GetOptions (
     "genome=s",
     "output=s",
     "help",
+    "debug|d",
     ) or pod2usage(2);
 
 pod2usage(1) if $options{'help'};
@@ -60,7 +61,7 @@ foreach my $st (keys %$refSt) {
     $hapST{'ST'. $st} = $refSt->{$st}
 }
 
-#print Dumper(\%hapST); exit;
+print Dumper(\%hapST) if $options{'debug'};
 # Read VCF file
 my @pos;
 open VCF, "<", $options{'vcf'};
@@ -75,15 +76,16 @@ close VCF;
 my $gIN = Bio::SeqIO->new(-file => $options{'genome'}, -format => 'genbank');
 my $ge = $gIN->next_seq();
 
-# Read BED data & create genome features
+# Read CSV data & create genome features
 my @gene_feats;
 while (<DATA>) {
     chomp;
-    my ($chr, $start, $end, $gid, $strand, $rest) = split;
-    push @gene_feats, Bio::SeqFeature::Generic->new(-seq_id => $gid, -start => $start, -end => $end, -strand => $strand);
+    next unless /^NC_/;
+    my ($chr, $start, $end, $gid, $strand, $rest) = split; 
+    push @gene_feats, Bio::SeqFeature::Generic->new(-seq_id => $gid, -start => $start, -end => $end, -strand => $strand eq 'plus' ? '+':'-');
 }
 
-#print Dumper(\@gene_feats); 
+print Dumper(\@gene_feats) if $options{'debug'}; 
 my @igs_feats;
 for (my $i=0; $i<$#gene_feats; $i++) {
     my $this = $gene_feats[$i];
@@ -94,7 +96,7 @@ for (my $i=0; $i<$#gene_feats; $i++) {
 						   -strand => $this->strand() . "|" . $next->strand(),
 	);    
 }
-#print Dumper(\@igs_feats); 
+print Dumper(\@igs_feats) if $options{'debug'};
 my @utr_feats;
 push @utr_feats, Bio::SeqFeature::Generic->new(-seq_id => '5-UTR', 
 					       -start => 1, 
@@ -106,7 +108,7 @@ push @utr_feats, Bio::SeqFeature::Generic->new(-seq_id => '3-UTR',
 					       -end => 29903, # end of NC_045512.2 
 					       -strand => 1);
 
-#print Dumper(\@utr_feats); exit;
+print Dumper(\@utr_feats) if $options{'debug'};
 #############################
 # Make a graph & calculate MST & polorize with root
 #############################
@@ -153,6 +155,7 @@ $traversal->dfs();
 # Call back function to process each edge using an anonymous sub
 my $polarize_an_edge = sub {
     my ($u, $v, $self) = @_;
+    if ($u eq $root) { warn "root position:", $u, "=>", $v, "\n"}
     my @diffs = &comp_seq($STs{$u}, $STs{$v});
     $mstg->set_edge_attribute($u, $v, 'change', \@diffs);
     $mstg->set_edge_attribute($u, $v, 'source', $u);
@@ -197,12 +200,13 @@ sub comp_seq { # possibly no change? due to missing base? yes; fix uniq_seq
 		    'src_base' => $i->subseq($k+1,$k+1), 
 		    'tgt_base' => $j->subseq($k+1,$k+1),
 		    'label' => $ref->[0],
-		    'length' => $ref->[1],
+#		    'length' => $ref->[1],
 		    'pos' => $ref->[2],
-		    'src_codon' => $ref->[3] || undef,
-		    'tgt_codon' => $ref->[4] || undef,
-		    'src_aa' => $ref->[5] || undef,
-		    'tgt_aa' => $ref->[6] || undef,
+		    'codon_pos' => $ref->[3] || undef,
+		    'src_codon' => lc($ref->[4]) || undef,
+		    'tgt_codon' => lc($ref->[5]) || undef,
+		    'src_aa' => $ref->[6] || undef,
+		    'tgt_aa' => $ref->[7] || undef,
 	}; # found a diff
     } 
     return @dif;
@@ -213,24 +217,79 @@ sub genome_info {
     my $feat = &genome_region($site);
     if ($feat->seq_id =~ /UTR/ || $feat->seq_id =~ /igs/) { # IGS or UTR site
 	return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1 ];
-    } else { # ORF site
+    } elsif ($feat->seq_id eq 'orf1ab') { # ORF shift
+	my $dna = Bio::Seq->new(-id=>'frag', -seq=>$ge->subseq($feat->start, $site));
+	my $dnaLen = $dna->length();
+	my $mod;
+	my $codonPos;
+	my ($codon1, $codon2, $aa1, $aa2);
+	if ($site < $orfShift) { # before the shift site
+	    $mod = $dnaLen % 3;
+	    if ($mod == 1) { # 1st codon position
+		$codon1 = $base1 . $ge->subseq($site+1, $site+2); 
+		$codon2 = $base2 . $ge->subseq($site+1, $site+2);
+		$codonPos = 1;
+	    } elsif ($mod == 2) {
+		$codon1 = $ge->subseq($site-1, $site-1) . $base1 . $ge->subseq($site+1, $site+1); 
+		$codon2 = $ge->subseq($site-1, $site-1) . $base2 . $ge->subseq($site+1, $site+1);
+		$codonPos = 2; 
+	    } else { # 3rd codon position
+		$codon1 = $ge->subseq($site-2, $site-1) . $base1; 
+		$codon2 = $ge->subseq($site-2, $site-1) . $base2; 
+		$codonPos = 3;
+	    }
+	    $aa1 = $myCodonTable->translate($codon1);
+	    $aa2 = $myCodonTable->translate($codon2);
+	    return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1, $codonPos, $codon1, $codon2, $aa1, $aa2 ];
+	} elsif ($site == $orfShift) {
+	    $mod = 0; # at the orf shift site
+	    $codon1 = $ge->subseq($site-2, $site-1) . $base1; 
+	    $codon2 = $ge->subseq($site-2, $site-1) . $base2; 
+	    $codonPos = 3;
+	    $aa1 = $myCodonTable->translate($codon1);
+	    $aa2 = $myCodonTable->translate($codon2);
+	    return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1, $codonPos, $codon1, $codon2, $aa1, $aa2 ];
+	} else { # after the shift site
+	    $mod = ($dnaLen + 1) % 3; # 1->2; 2->3; 3->1
+	    if ($mod == 1) { # 1st codon position
+		$codon1 = $base1 . $ge->subseq($site+1, $site+2); 
+		$codon2 = $base2 . $ge->subseq($site+1, $site+2);
+		$codonPos = 1;
+	    } elsif ($mod == 2) {
+		$codon1 = $ge->subseq($site-1, $site-1) . $base1 . $ge->subseq($site+1, $site+1); 
+		$codon2 = $ge->subseq($site-1, $site-1) . $base2 . $ge->subseq($site+1, $site+1);
+		$codonPos = 2; 
+	    } else { # 3rd codon position
+		$codon1 = $ge->subseq($site-2, $site-1) . $base1; 
+		$codon2 = $ge->subseq($site-2, $site-1) . $base2; 
+		$codonPos = 3;
+	    }
+	    $aa1 = $myCodonTable->translate($codon1);
+	    $aa2 = $myCodonTable->translate($codon2);
+	    return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1, $codonPos, $codon1, $codon2, $aa1, $aa2 ];
+	}
+    } else { # other ORFs
 	my $dna = Bio::Seq->new(-id=>'frag', -seq=>$ge->subseq($feat->start, $site));
 	my $dnaLen = $dna->length();
 	my $mod = $dnaLen % 3;
+	my $codonPos;
 	my ($codon1, $codon2, $aa1, $aa2);
 	if ($mod == 1) { # 1st codon position
 	    $codon1 = $base1 . $ge->subseq($site+1, $site+2); 
 	    $codon2 = $base2 . $ge->subseq($site+1, $site+2);
+	    $codonPos = 1;
 	} elsif ($mod == 2) {
 	    $codon1 = $ge->subseq($site-1, $site-1) . $base1 . $ge->subseq($site+1, $site+1); 
-	    $codon2 = $ge->subseq($site-1, $site-1) . $base2 . $ge->subseq($site+1, $site+1); 
+	    $codon2 = $ge->subseq($site-1, $site-1) . $base2 . $ge->subseq($site+1, $site+1);
+	    $codonPos = 2; 
 	} else { # 3rd codon position
 	    $codon1 = $ge->subseq($site-2, $site-1) . $base1; 
 	    $codon2 = $ge->subseq($site-2, $site-1) . $base2; 
+	    $codonPos = 3;
 	}
 	$aa1 = $myCodonTable->translate($codon1);
 	$aa2 = $myCodonTable->translate($codon2);
-	return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1, $codon1, $codon2, $aa1, $aa2 ];
+	return [ $feat->seq_id,  $feat->length, $site - $feat->start + 1, $codonPos, $codon1, $codon2, $aa1, $aa2 ];
     }
 }
 
@@ -243,20 +302,23 @@ sub genome_region {
 	next if $site > $fe->end;
 	$feat = $fe;
     }
+    die "feat not found: $site\n" unless $feat->seq_id;
 #    print $feat->seq_id, ":", $feat->start, ":", $feat->end, "\n"; 
     return $feat;
 }
 
 
 __DATA__
-NC_045512.2     265     21555   orf1ab  0       +
-NC_045512.2     21562   25384   S       0       +
-NC_045512.2     25392   26220   ORF3a   0       +
-NC_045512.2     26244   26472   E       0       +
-NC_045512.2     26522   27191   M       0       +
-NC_045512.2     27201   27387   ORF6    0       +
-NC_045512.2     27393   27759   ORF7a   0       +
-NC_045512.2     27755   27887   ORF7b   0       +
-NC_045512.2     27893   28259   ORF8    0       +
-NC_045512.2     28273   29533   N       0       +
-NC_045512.2     29557   29674   ORF10   0       +
+Accession       Start   Stop    Gene symbol     Strand  NCBI Gene ID    Name
+NC_045512.2     266     21555   orf1ab  plus    43740578
+NC_045512.2     21563   25384   S       plus    43740568
+NC_045512.2     25393   26220   ORF3a   plus    43740569
+NC_045512.2     26245   26472   E       plus    43740570
+NC_045512.2     26523   27191   M       plus    43740571
+NC_045512.2     27202   27387   ORF6    plus    43740572
+NC_045512.2     27394   27759   ORF7a   plus    43740573
+NC_045512.2     27756   27887   ORF7b   plus    43740574
+NC_045512.2     27894   28259   ORF8    plus    43740577
+NC_045512.2     28274   29533   N       plus    43740575
+NC_045512.2     29558   29674   ORF10   plus    43740576
+
