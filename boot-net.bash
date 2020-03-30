0a5dd92471076ee444334c9c394afc800e510534
weigangq <azureuser@myVM.qezgz301aduepadfdja3syg3uh.bx.internal.cloudapp.net>
2020-03-30 23:52:05 +0000

    add ref.gb

--- boot-net.bash
+++ boot-net.bash
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 # Usage: $0 <boot number>
-dir='boot-files';
+dir_file=$HOME/boot-files;
+dir_exe=$HOME/cov-browser;
 end=$1;
-# bootstrap alignment, build MST, and get edges
-perl hapnet.pl --genome ref.gb --vcf snps2-ref-03-19.vcf --hap imputed.aln --impute-log impute.log --output edge > $dir/net-0.edge
+
+#1 get edges for the original alignment ("imputed.aln")
+perl $dir_exe/hapnet.pl --genome $dir_exe/ref.gb --vcf s$dir_exe/snps-ref.vcf --hap $dir_exe/imputed.aln --impute-log $dir_exe/impute.log 2> log
+mv $dir_file/net.json $dir_file/net-0.json;
+mv $dir_file/edges.tsv $dir_file/edges-0.tsv;
+mv $dir_file/nodes.tsv $dir_file/nodes-0.tsv;
+cut -f1,2 $dir_file/edges-0.tsv | sort -u | tr '\t' '-' > $dir_file/edge-0;
+
+#2 get edges for bootstraped alignments
 for i in $(seq 1 $end); do 
     echo -ne "boot $i\t";
-    bioaln --boot imputed.aln > $dir/boot-$i.aln; 
-    perl hapnet.pl --genome ref.gb --vcf snps2-ref-03-19.vcf --hap $dir/boot-$i.aln --impute-log impute.log --output edge > $dir/net-$i.edge
-    cut -f1,2 $dir/net-$i.edge | sort -u | tr '\t' '-' > $dir/edge-$i; 
+    bioaln --boot imputed.aln > $dir_file/boot-$i.aln; 
+    perl $dir_exe/hapnet.pl --genome $dir_exe/ref.gb --vcf s$dir_exe/snp-ref.vcf --hap $dir_exe/boot-$i.aln --impute-log $dir_exe/impute.log 2> log
+    mv $dir_file/net.json $dir_file/net-$i.json;
+    mv $dir_file/edges.tsv $dir_file/edges-$i.tsv;
+    mv $dir_file/nodes.tsv $dir_file/nodes-$i.tsv;
+    cut -f1,2 $dir_file/edges-$i.tsv | sort -u | tr '\t' '-' > $dir_file/edge-$i; 
     echo -ne "done boot $i\n";
 done
 
-# obtain majority-rule parents
-cat $dir/edge-* | sort | uniq -c | sort -rn > unique-edges
-perl majority-parent.pl unique-edges > pa-cts
+#3 obtain majority-rule parents
+cat $dir_file/edge-* | sort | uniq -c | sort -rn > unique-edges
+perl $dir_exe/majority-parent.pl unique-edges > pa-cts
 exit;
