8021adcb3a3db4f0e8a5516c73a471721b976aa9
Saymon Akther <sakther@wallace.hunter.cuny.edu>
2020-04-08 21:44:20 -0400

    align-genome.bash: automated haplotype generator version 2

--- align-genome.bash
+++ align-genome.bash
@@ -2,12 +2,14 @@
 
 ## GENERATE HAPLOTYPES ##
 ## output: A vcf file and a fasta sequence file of haplotypes ##
-human_file=$1
-ref_seq=$2
-ploidy=$3
-environ_files=$4
 
-if [ $# -ne 4  ]
+human_file=$1 #a fasta file contain all the human cov genome sequences
+ref_seq=$2
+ploidy=$3 #vcf ploidy file
+environ_files=$4 #folder name that contain non-human cov genome bam files
+out_file=$5 #outgroup bam file (compressed)
+
+if [ $# -ne 5  ]
   then
     echo "Not enough arguments provided"
 	exit 1
@@ -75,46 +77,53 @@ vcftools --vcf snps.vcf --mac 2 --recode --recode-INFO-all --out snps2.vcf
 ## rename the snp2 recode file 
 mv snps2.vcf.recode.vcf snps2.vcf
 
+#step 1: index with gzip & tabix
+if [ -e snps2.vcf.gz ];then rm snps2.vcf.gz ; fi
+bgzip snps2.vcf
+tabix snps2.vcf.gz
+
+##fitler outgroup SNPs
+bcftools isec -n=2 -w1 $out_file snps2.vcf.gz > otg-isec-sites.vcf
+
+## index with gzip & tabix
+if [ -e otg-isec-sites.vcf.gz ];then rm otg-isec-sites.vcf.gz ; fi
+bgzip otg-isec-sites.vcf
+tabix otg-isec-sites.vcf.gz
+
+## merge (no duplicated line; add ref state "0" instead of missing)
+vcf-merge -d -R 0 otg-isec-sites.vcf.gz snps2.vcf.gz > snps-ref.vcf
+
+## check stats 
+bcftools stats snps-ref.vcf > stats
+grep "number of samples" stats >> human.log
+
+##rename sequecnes 
+sed -e 's/\(human\/\|environ\/\|.sorted.bam\)//g' snps-ref.vcf > snps2-ref.vcf
+
 ## Get sample FASTA
-bcftools query -l snps2.vcf > samples
-cat samples | while read line; do echo ">$line"; bcftools query -s "$line" -f '[%TGT]' snps2.vcf; echo; done > samples.fas
+bcftools query -l snps2-ref.vcf > samples
+cat samples | while read line; do echo ">$line"; bcftools query -s "$line" -f '[%TGT]' snps2-ref.vcf; echo; done > samples.fas
 
 ## check the number of samples in fas file and vcf file
 bioseq -n samples.fas >> human.log
-bcftools stats snps2.vcf > stats 
+bcftools stats snps2-ref.vcf > stats 
 grep "number of samples" stats >> human.log
-
-#vcftools --vcf --counts snps.vcf
-
-## to get ref FASTA; if necessary
-#echo ">ref" >> samples.fas
-#grep "^CP" gbs50-snps2.vcf | cut -f4 | paste -s -d '' >> sample.fas)
-
-#rename sequence
-#cat samples.fas | sed "s/EPI_IS//; s/.sorted.bam//" > samples2.fas
-#cat samples.fas | sed "s/.sorted.bam//" > samples2.fas
-cat samples.fas | sed "s|^[^/]*/|>|g; s/.sorted.bam//" > samples2.fas
 
 #get max haplotype 
 max=$(bioseq -g samples.fas | bioseq -l | sort -k2 -nr | less | head -1 | cut -f2)
-cutoff=$(echo $(( max*20/100 )))
+cutoff=$(echo $(( max*10/100 )))
+echo "ambig cutoff is $cutoff" >> human.log
 
 ## remove seqs with 20% of missing/non-ATCG bases "."
-#bioseq -d'ambig:20' samples2.fas > samples3.fas
 if [ $cutoff -gt 1 ]
 then
-	bioseq -d "ambig:$cutoff" samples2.fas > samples3.fas
+	bioseq -d "ambig:$cutoff" samples.fas > samples2.fas
 else 
-	bioseq -d "ambig:1" samples2.fas > samples3.fas
+	bioseq -d "ambig:1" samples.fas > samples2.fas
 fi
-
-#cat samples2.fas | grep "N" | grep -v
-
-bioaln -i'fasta' --gap-char '-' samples3.fas  > samples4.aln
 
 # change gap to "-" (internal gaps counted as different)
 #bioaln -i'fasta' --gap-char '-' samples3.fas  > samples4.aln
 
 # change gaps to "n" to help reducing unique haps
-bioaln -i'fasta' --gap-char 'n' samples3.fas  > samples4.aln
-
+bioaln -i'fasta' --gap-char 'n' samples2.fas  > samples3.aln
